9f7470349d73c3805902f90c5e545952bd51de65
Willie Rogers <wjrogers@nih.gov>
2016-10-27 13:40:58 -0400

    Moved location of UMLS input files for prepumls.sh
    Program now looks for UMLS input files in data/input instead of input.

--- bin/prepumls.sh
+++ bin/prepumls.sh
@@ -13,23 +13,23 @@ mkdir -p data/ivf/${DSNAME}/tables data/ivf/${DSNAME}/ifindices
 # Generate abbreviated version of MRSTY.RRF
 if [ ! -e data/ivf/${DSNAME}/tables/zzsty ]; then
     echo "Converting MRSTY.RRF to zzsty"
-    gawk 'BEGIN { FS="|" } { printf("%s|%s\n", $1, $4) }' input/${DSNAME}/MRSTY.RRF > data/ivf/${DSNAME}/tables/zzsty
+    gawk 'BEGIN { FS="|" } { printf("%s|%s\n", $1, $4) }' data/input/${DSNAME}/MRSTY.RRF > data/ivf/${DSNAME}/tables/zzsty
 fi
 
 # link to full version of MRSTY.RRF
 if [ ! -e data/ivf/${DSNAME}/tables/mrsty.rrf ]; then
-    ln -s input/${DSNAME}/MRSTY.RRF data/ivf/${DSNAME}/tables/mrsty.rrf
+    (cd data/ivf/${DSNAME}/tables && ln -s ../../../input/${DSNAME}/MRSTY.RRF mrsty.rrf )
 fi
 
 # Keep only English rows of MRCONSO.RRF
 if [ ! -e data/ivf/${DSNAME}/tables/mrconso.eng ]; then
-    gawk -F \| '$2 ~ /ENG/' input/${DSNAME}/MRCONSO.RRF > data/ivf/${DSNAME}/tables/mrconso.eng
+    gawk -F \| '$2 ~ /ENG/' data/input/${DSNAME}/MRCONSO.RRF > data/ivf/${DSNAME}/tables/mrconso.eng
 fi
 
 # link to full version of MRSAT.RRF
-if [ ! -e data/ivf/${DSNAME}/tables/mrsat.rrf ]; then
-    ln -s input/${DSNAME}/MRSAT.RRF data/ivf/${DSNAME}/tables/mrsat.rrf
-fi
+# if [ ! -e data/ivf/${DSNAME}/tables/mrsat.rrf ]; then
+#    ln -s data/input/${DSNAME}/MRSAT.RRF data/ivf/${DSNAME}/tables/mrsat.rrf
+# fi
 
 # create inverted file configuraiion file
 echo "NUM_TABLES: 6
@@ -44,11 +44,12 @@ mrsty.rrf|mrstyrrf|6|0|cui|tui|stn|sty|atui|cvf|TXT|TXT|TXT|TXT|TXT|TXT
 # build index after tables are generated
 # setenv HEAPSIZE -Xmx1500m
 export HEAPSIZE=-Xmx20000m
-export CLASSPATH=lib/irutils-2.0-SNAPSHOT.jar
+#export CLASSPATH=lib/irutils-2.0-SNAPSHOT.jar
+export CLASSPATH=target/ctb-0.1.0-SNAPSHOT-standalone.jar
 export PROJECTROOT=data/ivf/${DSNAME}
 
 # for dbname in mrsty mrconso mrconsostr mrstyrrf mrsat ; do
-for dbname in mrsty mrconso mrconsostr mrstyrrf mrsat ; do
+for dbname in mrsty mrconso mrconsostr mrstyrrf ; do
     echo "Building index for $DSNAME/$dbname."
     java -Dindex.path=${PROJECTROOT}/ifindices \
 	 -Dtable.path=${PROJECTROOT}/tables \
