18fb39b55d0be3313e4765ec0290d5850814c754
Willie Rogers <wjrogers@nih.gov>
2020-05-07 13:33:18 -0400

    Updated dependencies in project file.
    Also set logging-factory method in :jvm-opts for use when invoked as
    standalone application; moved servlet-api dependencies to :dev
    dependencies.

--- project.clj
+++ project.clj
@@ -8,28 +8,31 @@
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.zip "0.1.2"]
-                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [digest "1.4.4"]
                  [hiccup "1.0.5"]
                  [compojure "1.6.1"]
-                 [ring/ring-devel "1.6.1"]
-                 [ring/ring-core "1.6.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-devel "1.8.1"]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-jetty-adapter "1.8.0"]
                  [ring-basic-authentication "1.0.5"]
-                 [javax.servlet/servlet-api "2.5"]
-                 [javax.servlet/javax.servlet-api "3.1.0" :scope "provided"]
                  [skr "0.1.2-SNAPSHOT"]
                  [irutils "2.0"]
                  [umls-tables "0.1.0-SNAPSHOT"]
                  [lvgclj "0.1.0-SNAPSHOT"]
-                 [org.lpetit.ring/ring-java-servlet "0.2.0"]]
+                 [org.lpetit.ring/ring-java-servlet "0.2.0"]
+                 [clj-time "0.15.2"]
+                 [log4j/log4j "1.2.17"]]
+  :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler ctb.webapp/app
          :web-xml "web.xml"}
-  :repositories [["ii" "https://metamap.nlm.nih.gov/maven2/"]]
+  :repositories [["ii" "https://metamap.nlm.nih.gov/maven2/"]
+                 ["jitpack" "https://jitpack.io"]]
   :marginalia {:javascript ["mathjax/MathJax.js?config=default"]}
   :min-lein-version "2.5.0"
   :target-path "target/%s"
+  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/log4j-factory"]
   :aot [ctb.core
         ctb.process-config
         ctb.ctblistener
